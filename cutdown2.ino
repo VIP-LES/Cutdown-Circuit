e97bf5a1b665e8aa98d7cdeb7c6f0e36f94a4a45
Vishnu <kaushik.gc98@gmail.com>
2019-12-03 13:54:23 -0500

    Update cutdown2.ino
    Modified the code to reflect the new timer settings and the relay settings (active LOW).

--- cutdown2.ino
+++ cutdown2.ino
@@ -1,86 +1,61 @@
-//this is all we need to grab the data from the serial port and put it through the publisher
-String content = "";
-char character;
-
-
-// Takes 8 seconds to ensure cutdown?
+// Takes 8 s to ensure cutdown:
 int CUTDOWN_TIME_MILLIS = 8000;
-Timer ctimer(8000, deactivate_circ);
+Timer ctimer(CUTDOWN_TIME_MILLIS, deactivate_circ);
 
-// alternate timer cutdown:
-// https://docs.particle.io/reference/device-os/firmware/photon/#start-
-int periodicity = 60000; // call every 60,000ms = 60s = 1 min
-Timer timer(periodicity, time_to_cutdown);
-int CUTDOWN_MINS_LIMIT = 180; // cutdown after 180 mins = 3 hrs
+
+// Timed cutdown mechanism, cutdown after 3.5 hours.
+int hours = 7 * 60 * 60 * 1000 / 2;
+Timer hourtimer(hours, activate_hour);
+
 
 void setup() {
-  Serial.begin(9600);   //this is the usb port at 9600
   
+  // The cutdown pins need to be configured first to let them
+  // act as power sources. Connect the relay channels to the
+  // Particle Board's pins as follows:
+  // IN1 -> B0
+  // IN2 -> B1
+  // VCC -> VIN (top right of board, next to -il, i.e. GROUND)
+  // GND -> -il (symbol for GROUND)
   
-  // The cutdown pin need to be configured first to let it act as a power source.
+  hourtimer.reset();
+  
   pinMode(B0, OUTPUT);
   pinMode(B1, OUTPUT);
-  Particle.function("cutcirc2", activate_cutdown);
+  pinSetFast(B0);
+  pinSetFast(B1);
   
-  timer.start();
+  Particle.function("cutcirc2", activate_cutdown2);
+  Particle.function("resethour", reset_hourtimer);
+  
 }
 
 void loop() {
-
+    locator.loop();
 }
 
 
-void time_to_cutdown() {
-    static int hit_cutdown = 0;
-    static int plus = 1;
-    
-    hit_cutdown += plus;
-    
-    if (hit_cutdown >= CUTDOWN_MINS_LIMIT) {
-        hit_cutdown = 0;
-        plus = 0;
-        activate_cutdown(NULL);
-    }
+int reset_hourtimer(const char* msg) {
+    hourtimer.reset();
+    return 13;
 }
 
-
-void serialEvent()  //built in callback from serial
-{
-  while (Serial.available()) {
-      character = Serial.read();//reads character by character
-      content.concat(character);//dumps into a string that'll deal with the variable size on the backend
-  }
-  Particle.publish("data", content, PRIVATE); //We are publishing all in one topic to save a few bytes
-  content = "";
+void activate_hour() {
+    activate_cutdown2(0);
+    hourtimer.stop();
 }
 
-
-// curl https://api.par373037383634/cutcirc2 -d access_token=b38d047a9da4e6b5e60f8a6f62814bdb0ee8267a
-
-// This is a cheap cutdown circuit method, since it simply activates, waits, and
-// deactivates. I think there is an issue here: this can't be triggered through
-// an nterrupt, since the delay function blocks (I think it blocks).
-int activate_cutdown(const char* msg) {
-	// Set the cutdown pin to HIGH for some time, and then set it to low again.
-	pinSetFast(B0);
-	pinSetFast(B1);
-	delay(CUTDOWN_TIME_MILLIS);
-	pinResetFast(B0);
-	pinResetFast(B1);
-	return 13;
-}
-
-
-// an alternate cutdown circuit approach, will test later. For now, the above works.
+// Remote control activated cutdown. Hit via console.particle.io through
+// the manual "call fuction" capability.
 int activate_cutdown2(const char* msg) {
-    pinSetFast(B0);
-    pinSetFast(B1);
     ctimer.reset(); // starts if stopped
+    pinResetFast(B0);
+    pinResetFast(B1);
     return 11;
 }
 
 void deactivate_circ() {
-    pinResetFast(B0);
-    pinResetFast(B1);
+    pinSetFast(B0);
+    pinSetFast(B1);
     ctimer.stop();
 }
